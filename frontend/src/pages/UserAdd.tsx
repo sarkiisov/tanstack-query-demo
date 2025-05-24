20b441372283e7eef7bd2cb5e0bc945b9bf7924f
sarkiisov <sarkisov1337@gmail.com>
2025-05-24 14:38:10 +0300

    feat: redirect after add user

--- frontend/src/pages/UserAdd.tsx
+++ frontend/src/pages/UserAdd.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router'
 import { UserForm } from '../components/UserForm/UserForm'
 import { useCreateUser } from '../hooks/useCreateUser'
 import type { User } from '../types'
@@ -5,11 +6,14 @@ import type { User } from '../types'
 export const UserAdd = () => {
   const createUserMutation = useCreateUser()
 
-  const handleFormSubmit = (user: Omit<User, 'id'>) => {
-    createUserMutation.mutate({
-      id: crypto.randomUUID(),
-      ...user
-    })
+  const navigate = useNavigate()
+
+  const handleFormSubmit = async (user: Omit<User, 'id'>) => {
+    const id = crypto.randomUUID()
+
+    await createUserMutation.mutateAsync({ id, ...user })
+
+    navigate(`/users/${id}`)
   }
 
   return <UserForm onSubmit={handleFormSubmit} />
